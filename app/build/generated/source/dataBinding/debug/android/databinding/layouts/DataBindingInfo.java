af858b24cc918baa7a3a2971e66236b59074d1fb
fawzy <ma7madfawzy@gmail.com>
2018-05-20 15:24:28 +0200

    logout API access

--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="c4cf209c-d9cb-435e-aabe-0a928a615b84")
+@BindingBuildInfo(buildId="dac10e44-e528-429c-8091-32025c0a591d")
 public class DataBindingInfo {}
--- app/src/main/java/yackeen/com/daleel/chat/ChattingActivity.java
+++ app/src/main/java/yackeen/com/daleel/chat/ChattingActivity.java
@@ -176,6 +176,7 @@ public class ChattingActivity extends AppCompatActivity {
     }
 
     private void updateRecycler() {
+        Log.d(TAG,"listSize= "+messagesList.size());
         noDataFound.setVisibility(messagesList.size() > 0?View.GONE:View.VISIBLE);
         adapter.setList(messagesList);
     }
